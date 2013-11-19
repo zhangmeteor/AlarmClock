a35c7ff8c00cc055e306c8e03f184b2f93e510d5
zhang <438114643@qq.com>
2013-11-19 12:51:26 +0800

    Initial Commit

---
+++ Alarm/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Alarm
+//
+//  Created by zhanghao on 13-11-19.
+//  Copyright (c) 2013年 zhanghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Alarm/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Alarm
+//
+//  Created by zhanghao on 13-11-19.
+//  Copyright (c) 2013年 zhanghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
